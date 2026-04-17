341139a0e02d428ed31d2bdda4f34d8419805c90
dudiday <dudi199784@gmail.com>
2026-04-17 22:31:57 +0900

    add RsData

---
+++ src/main/java/com/rest/global/rsData/RsData.java
@@ -0,0 +1,35 @@
+package com.rest.global.rsData;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Service;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class RsData<T> {
+    private String resultCode;
+    private String resultMessage;
+    private T data;
+
+    public static <T> RsData<T> of(String resultCode, String resultMessage, T data) {
+        return new RsData<T>(resultCode, resultMessage, data);
+    }
+
+    public static <T> RsData<T> of(String resultCode, String resultMessage) {
+        return new RsData<T>(resultCode, resultMessage, null);
+    }
+
+    @JsonIgnore
+    public boolean isSuccess() {
+        return resultCode.startsWith("S-");
+    }
+
+    @JsonIgnore
+    public boolean isFail(){
+        return !isSuccess();
+    }
+
+}
