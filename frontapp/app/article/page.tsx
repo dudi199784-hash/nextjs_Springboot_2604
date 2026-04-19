6fe9fa0195771f52b614d0d7215afb6cae4b76a6
dudiday <dudi199784@gmail.com>
2026-04-19 18:50:32 +0900

    front edit articles page

--- frontapp/app/article/page.tsx
+++ frontapp/app/article/page.tsx
@@ -1,8 +1,36 @@
-export default async function Article() {
-    await fetch("http://localhost:8090/api/v1/articles")
-    return (
+'use client'
+
+import { useEffect, useState } from 'react'
+
+
+export default function ArticleDetail() {
+    const [articles, setArticles] = useState([]);
+
+    useEffect (() => {
+        getData()
+    }, [])
+
+    const getData = async () => {
+        const result = await fetch("http://localhost:8090/api/v1/articles").then(row => row.json());
+        setArticles(result.data.articles);
+        console.log(result.data.articles);
+    }
+
+    return(
         <div>
-            <div>게시판 22</div>
-        </div>
+            <h4>번호 / 제목 / 생성일</h4>
+            {articles.length == 0 ? (
+                <p>현재 게시무이 없습니다.</p>
+            ) : (
+                <ul>
+                    {articles.map(article => 
+                        <li key={article.id}>
+                            {article.id}/{article.subject}/{article.createDate}
+                        </li>
+                    )}
+                </ul>    
+            )}
+        </div>    
+        
     );
 }
