09086932da0f0451c1b586d02c29cb45dbc38195
dudiday <dudi199784@gmail.com>
2026-04-20 12:03:20 +0900

    add jwt provider

---
+++ src/main/java/com/rest/global/util/Util.java
@@ -0,0 +1,25 @@
+package com.rest.global.util;
+
+import tools.jackson.databind.ObjectMapper;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class Util {
+    public static class json{
+        public static Object toStr(Map<String, Object> map) {
+            try {
+                return new ObjectMapper().writeValueAsString(map);
+            }catch (Exception e){
+                return null;
+            }
+        }
+    }
+    public static Map<String, Object> toMap(String jsonStr) {
+        try {
+            return new ObjectMapper().readValue(jsonStr, LinkedHashMap.class);
+        }catch (Exception e){
+            return null;
+        }
+    }
+}
