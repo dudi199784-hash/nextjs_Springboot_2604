6cb8e77ab58fa4d76bb1c223bb543e9b76b71c05
dudiday <dudi199784@gmail.com>
2026-04-20 20:15:50 +0900

    add jwt filter

--- src/main/java/com/rest/global/security/ApiSecurityConfig.java
+++ src/main/java/com/rest/global/security/ApiSecurityConfig.java
@@ -1,16 +1,24 @@
 package com.rest.global.security;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+
+@Slf4j
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class ApiSecurityConfig {
+    private final JwtAuthorizationFilter jwtAuthorizationFilter;
+
     @Bean
     SecurityFilterChain apiFilterChain(HttpSecurity http) throws Exception {
         http
@@ -19,12 +27,28 @@ public class ApiSecurityConfig {
                         auth -> auth
                                 .requestMatchers("/api/*/articles").permitAll()
                                 .requestMatchers("/api/*/articles/*").permitAll()
+                                .requestMatchers(HttpMethod.POST,"/api/*/members/login").permitAll()
                                 .anyRequest().authenticated()
                 )
                 .csrf(
                         csrf -> csrf
                                 .disable()
-                );
+                ) // csrf 토큰 끄기
+                .httpBasic(
+                        httpBasic -> httpBasic.disable()
+                ) // httpBasic 로그인 방식 끄기
+                .formLogin(
+                        formLogin -> formLogin.disable()
+
+                ) // 폼 로그인 방식 끄기
+                .sessionManagement(
+                    sessionManagement -> sessionManagement.sessionCreationPolicy(STATELESS)
+                ) // 세션 끄기
+                .addFilterBefore(
+                    jwtAuthorizationFilter, // 엑세스 토큰을 이용한 로그인 처리
+                        UsernamePasswordAuthenticationFilter.class
+        )
+        ;
         return http.build();
     }
 }
---
+++ src/main/java/com/rest/global/security/JwtAuthorizationFilter.java
@@ -0,0 +1,30 @@
+package com.rest.global.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthorizationFilter extends OncePerRequestFilter {
+    @Override
+    @SneakyThrows
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain){
+        if (request.getRequestURI().equals("/api/v1/members/login") ||
+                request.getRequestURI().equals("/api/v1/members/logout")) {
+            filterChain.doFilter(request,response);
+        }
+
+        String accessToken = "";
+
+        if ( !accessToken.isBlank() ){
+
+        }
+
+        filterChain.doFilter(request,response);
+    }
+}
